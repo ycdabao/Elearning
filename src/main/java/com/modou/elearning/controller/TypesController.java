b4e658da4748b130af55b3e1805b3c8e6a350795
ycdabao <ycdabao@yeah.net>
2018-08-21 14:44:39 +0800

    Merge branch 'master' of https://github.com/ycdabao/Elearning
    # Conflicts:
    #	src/main/resources/templates/admin/file.html

--- src/main/java/com/modou/elearning/controller/TypesController.java
+++ src/main/java/com/modou/elearning/controller/TypesController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Random;
 
 @Controller
 @RequestMapping(value="/types")
